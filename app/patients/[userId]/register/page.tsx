fbe7a40ef61406fc4ee038494344bb657f8c6362
CjSong97 <songcj2010@gmail.com>
2024-12-29 11:06:33 +0000

    first commit

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,14 +1,14 @@
-import PatientForm from "@/components/forms/PatientForm";
-import { PasskeyModal } from "@/components/PasskeyModal";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import RegisterForm from '@/components/forms/RegisterForm'
+import { getUser } from '@/lib/actions/patient.actions'
 
-const Home = async ({ searchParams }: SearchParamProps) => {
-  const currentSearchParams = await searchParams;
-  const isAdmin = currentSearchParams?.admin === "true";
+const Register = async({ params}: SearchParamProps) => {
+  const {userId} = await params;
+    const user = await getUser(userId);
   return (
     <div className="flex h-screen max-h-screen">
-      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
@@ -19,27 +19,26 @@ const Home = async ({ searchParams }: SearchParamProps) => {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm />
+          <RegisterForm user={user}/>
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
             © 2024 CarePulse
             </p>
-            <Link href="/?admin=true" className="text-green-500">
+            <Link href="/?admin-true" className="text-green-500">
               Admin
             </Link> 
           </div>
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/register-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]"
+        className="side-img max-w-[390px]"
       />
     </div>
-    
   )
 }
 
-export default Home;
+export default Register
